0be2a1047015188e2a6ff409fbba1b8a62f42c98
mrzen33 <mrzen33@outlook.com>
2024-09-12 01:56:14 +0930

    nav bar

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -3,9 +3,25 @@
 export default function App() {
   return (
     <>
-      <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
-  
+      <nav className="navbar border-bottom border-body">
+        <div className="container">
+          <a className="navbar-brand mb-0 h1" href="/">Weather App</a>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
       </nav>
+
+      <div className=" justify-content-center d-flex me-2" >
+        <div className="card" style={{ width: "28rem" }}>
+
+          <div className="card-body">
+            <p className="card-text"></p>
+          </div>
+        </div>
+
+      </div>
     </>
   )
 }
