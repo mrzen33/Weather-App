8e687cc0a5dae28090514087e5bc7fee0e59e56f
mrzen33 <mrzen33@outlook.com>
2024-09-12 02:44:52 +0930

    redesign and remake: remove nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Merriweather_Sans } from 'next/font/google'
 import 'bootstrap/dist/css/bootstrap.css'; // use bootstrap
 
 export const metadata: Metadata = {
@@ -6,14 +7,16 @@ export const metadata: Metadata = {
   description: "",
 };
 
+const my_font = Merriweather_Sans({ subsets: ['latin'] });
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" >
+      <body className={my_font.className}>
         {children}
       </body>
     </html>
--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,27 +1,15 @@
-
+"use client";
+import { useState } from "react";
 
 export default function App() {
+
+
   return (
-    <>
-      <nav className="navbar border-bottom border-body">
-        <div className="container">
-          <a className="navbar-brand mb-0 h1" href="/">Weather App</a>
-          <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Search</button>
-          </form>
-        </div>
-      </nav>
-
-      <div className=" justify-content-center d-flex me-2" >
-        <div className="card" style={{ width: "28rem" }}>
-
-          <div className="card-body">
-            <p className="card-text"></p>
-          </div>
-        </div>
-
+    <div className="vw-100 vh-100">
+      <div className="d-flex justify-content-center">
+        <h1 className="mt-4">Weather App</h1>
+        
       </div>
-    </>
-  )
+    </div>
+  );
 }
